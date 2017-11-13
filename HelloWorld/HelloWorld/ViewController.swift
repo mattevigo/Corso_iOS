339c72548b29657e75a8aef5263bcefe546ee5c2
Matteo Vigoni <mattevigo@gmail.com>
2017-11-13 21:48:18 +0100

    Creazione progetto iOS “Hello World!”

---
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Matteo Vigoni on 13/11/2017.
+//  Copyright © 2017 BioBeats. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
